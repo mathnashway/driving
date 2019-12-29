fc4b6e9cfd3d8732a04e4f847df51e81a38839fd
nashway <mathnashway@gmail.com>
2019-12-29 15:50:07 +0800

    Add

---
+++ driving.py
@@ -0,0 +1,13 @@
+country = input('哪國人: ')
+age = input('請輸入年齡: ')
+age = int(age)
+if country =='台灣':
+	if age >=18:
+		print('你可以考駕照')
+	else:
+		print('你不可以考駕照')
+elif country =='美國':
+	if age >=18:
+		print('你可以考駕照')
+	else:
+		print('你不可以考駕照')
